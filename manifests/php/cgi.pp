38606d14150de1c8f2bfbff420fe8840ac1b1769
Thomas Sturm <ths@nerdli.ch>
2014-04-15 18:34:20 +0200

    fix deprecated variable notation in template

--- manifests/php/cgi.pp
+++ manifests/php/cgi.pp
@@ -105,8 +105,8 @@ class nginxpack::php::cgi (
       path    => '/etc/php5/cgi/php.ini',
       match   => 'post_max_size',
       line    => inline_template('post_max_size = <%= \
-        (upload_max_files.to_i * upload_max_filesize[0..-2].to_i).to_s\
-        + upload_max_filesize[-1] %>'),
+        (@upload_max_files.to_i * @upload_max_filesize[0..-2].to_i).to_s\
+        + @upload_max_filesize[-1] %>'),
       require => Package['php5-cgi'],
       notify  => Service['php-fastcgi'],
     }
